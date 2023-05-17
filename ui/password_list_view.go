802a2872b42996da34d86e15a2ebc7d2bb7350bf
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-05-17 22:17:37 +0100

    implemented list view

--- ui/password_list_view.go
+++ ui/password_list_view.go
@@ -2,28 +2,84 @@ package ui
 
 import (
 	"fmt"
-	s "muscurdig/state"
+	c "muscurdig/context"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
-func GetPasswordListView(state *s.AppState) *fyne.Container {
+func GetPasswordListView(ctx *c.AppContext) *fyne.Container {
+	passwordCount := ctx.Db.GetPasswordCount()
 	searchEntry := widget.NewEntry()
-	content := container.NewCenter(widget.NewLabel("0 Password Entries"))
-	searchBtn := widget.NewButton("Search", func() {
-		fmt.Println("Searched")
+	content := container.New(layout.NewMaxLayout(), container.NewCenter(widget.NewLabel(fmt.Sprintf("%d Password Entries", passwordCount))))
+	searchBtn := widget.NewButtonWithIcon("Search", theme.SearchIcon(), func() {
+		passwords := ctx.Db.GetAllPasswords()
 		content.RemoveAll()
-		content.Add(container.NewCenter(widget.NewLabel("1 Password Entries")))
+		content.Add(
+			// TODO: need to do this a Bindable List so removing items will refresh here too
+			widget.NewList(
+				func() int { return len(passwords) },
+				getPasswordEntryRow,
+				func(lii widget.ListItemID, co fyne.CanvasObject) {
+					label, btnView, btnEdit, btnDelete := getListWidgetsFromContainer(&co)
+					pe := passwords[lii]
+					label.SetText(pe.Website)
+
+					btnView.OnTapped = func() {
+						ctx.NavigateToWithParam(c.Details, pe.Id)
+					}
+
+					btnEdit.OnTapped = func() {
+						ctx.NavigateToWithParam(c.AddUpdate, pe.Id)
+					}
+					btnDelete.OnTapped = func() {
+						ctx.Db.DeletePasswordEntry(pe.Id)
+						ctx.NavigateTo(c.List)
+					}
+				},
+			),
+		)
 	})
 
 	return container.New(layout.NewMaxLayout(),
 		container.NewBorder(
 			container.NewGridWithColumns(2, searchEntry, searchBtn),
-			nil, nil, nil,
+			container.NewBorder(nil, nil, nil, widget.NewButtonWithIcon("Add", theme.ContentAddIcon(), func() {
+				ctx.NavigateTo(c.AddUpdate)
+			})),
+			nil, nil,
 			content,
 		),
 	)
+}
+
+func getPasswordEntryRow() fyne.CanvasObject {
+	return container.NewGridWithColumns(3,
+		widget.NewLabel(""),
+		container.NewMax(),
+		container.NewBorder(
+			nil,
+			nil,
+			nil,
+			container.NewHBox(
+				widget.NewButtonWithIcon("", theme.FileTextIcon(), func() {}),
+				widget.NewButtonWithIcon("", theme.DocumentCreateIcon(), func() {}),
+				widget.NewButtonWithIcon("", theme.DeleteIcon(), func() {}),
+			),
+		),
+	)
+}
+func getListWidgetsFromContainer(co *fyne.CanvasObject) (*widget.Label, *widget.Button, *widget.Button, *widget.Button) {
+	container := (*co).(*fyne.Container)
+	label := container.Objects[0].(*widget.Label)
+	btnBoxWrapper := container.Objects[2].(*fyne.Container)
+	btnBox := btnBoxWrapper.Objects[0].(*fyne.Container)
+	btnView := btnBox.Objects[0].(*widget.Button)
+	btnEdit := btnBox.Objects[1].(*widget.Button)
+	btnDelete := btnBox.Objects[2].(*widget.Button)
+
+	return label, btnView, btnEdit, btnDelete
 }
