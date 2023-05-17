887ed266ea5fb0df83dc10153a1877ae1ba70001
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-05-17 22:14:12 +0100

    added clover db

--- db/db.go
+++ db/db.go
@@ -1,17 +1,140 @@
 package db
 
 import (
-	"fmt"
-	"muscurdig/conf"
+	"errors"
+	"muscurdig/libs"
 	"muscurdig/models"
-	"os"
+
+	c "github.com/ostafen/clover/v2"
 )
 
-func GetMasterPassword() models.MasterPassword {
-	data, err := os.ReadFile(fmt.Sprintf("./%s/pwd", conf.DbFiles))
+const (
+	masterPasswordCollection = "MASTER_PASSWORDS"
+	passwordEntryCollection  = "PASSWORD_ENTRIES"
+	cryptoCacheKey           = "cryptoCacheKey"
+)
+
+func setupCollections(db *c.DB) {
+	db.CreateCollection(masterPasswordCollection)
+	db.CreateCollection(passwordEntryCollection)
+}
+
+type Db struct {
+	clover *c.DB
+	cache  map[string]any
+}
+
+func NewDb(dbFiles string) *Db {
+	c, err := c.Open(dbFiles)
 	if err != nil {
 		panic(err)
 	}
 
-	return models.NewMasterPasswordFromB64(string(data))
+	setupCollections(c)
+	cached := map[string]any{}
+
+	return &Db{c, cached}
+}
+
+func (db *Db) Close() error {
+	return db.clover.Close()
+}
+
+func (db *Db) SaveMasterPassword(mp models.MasterPassword) (models.MasterPassword, error) {
+	doc := c.NewDocumentOf(mp)
+	id, err := db.clover.InsertOne(masterPasswordCollection, doc)
+	if err != nil {
+		panic(err)
+	}
+
+	mp.Id = id
+
+	return mp, nil
+}
+
+func (db *Db) GetMasterPassword() (models.MasterPassword, error) {
+	if mp, ok := db.cache[masterPasswordCollection]; ok {
+		return mp.(models.MasterPassword), nil
+	}
+
+	doc, err := db.clover.FindFirst(c.NewQuery(masterPasswordCollection))
+	if doc == nil {
+		return models.MasterPassword{}, errors.New("No MasterPassword stored")
+	}
+	if err != nil {
+		panic(err)
+	}
+	var mp models.MasterPassword
+	err = doc.Unmarshal(&mp)
+	if err != nil {
+		panic(err)
+	}
+
+	db.cache[masterPasswordCollection] = mp
+
+	return mp, nil
+}
+
+func (db *Db) GetCryptoInstance() *libs.Crypto {
+	if instance, ok := db.cache[cryptoCacheKey]; ok {
+		return instance.(*libs.Crypto)
+	}
+	mp, _ := db.GetMasterPassword()
+	instance := mp.GetCrypto()
+	db.cache[cryptoCacheKey] = &instance
+
+	return &instance
+}
+
+func (db *Db) InsertPasswordEntry(password models.PasswordEntry) error {
+	crypto := db.GetCryptoInstance()
+
+	passDto := password.DTO(crypto)
+
+	doc := c.NewDocumentOf(passDto)
+	_, err := db.clover.InsertOne(passwordEntryCollection, doc)
+
+	return err
+}
+
+func (db *Db) GetPasswordCount() int {
+	c, _ := db.clover.Count(c.NewQuery(passwordEntryCollection))
+
+	return c
+}
+
+func (db *Db) GetPasswordById(id string) models.PasswordEntry {
+	crypto := db.GetCryptoInstance()
+	doc, _ := db.clover.FindById(passwordEntryCollection, id)
+	dto := loadPasswordEntryDto(doc)
+	return dto.ToPasswordEntry(crypto)
+
+}
+
+func (db *Db) DeletePasswordEntry(id string) {
+	db.clover.DeleteById(passwordEntryCollection, id)
+}
+func (db *Db) UpdatePasswordEntry(pe models.PasswordEntry) error {
+	dto := pe.DTO(db.GetCryptoInstance())
+	doc := c.NewDocumentOf(dto)
+	return db.clover.UpdateById(passwordEntryCollection, pe.Id, doc.ToMap())
+}
+
+func (db *Db) GetAllPasswords() []models.PasswordEntry {
+	pwDtosDocs, _ := db.clover.FindAll(c.NewQuery(passwordEntryCollection))
+	crypto := db.GetCryptoInstance()
+	result := make([]models.PasswordEntry, len(pwDtosDocs))
+	for i, doc := range pwDtosDocs {
+		dto := loadPasswordEntryDto(doc)
+		result[i] = dto.ToPasswordEntry(crypto)
+	}
+
+	return result
+}
+
+func loadPasswordEntryDto(doc *c.Document) *models.PasswordEntryDto {
+	var dto models.PasswordEntryDto
+	doc.Unmarshal(&dto)
+	dto.Id = doc.ObjectId()
+	return &dto
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,18 @@ go 1.19
 
 require (
 	fyne.io/fyne/v2 v2.3.4
+	github.com/ostafen/clover/v2 v2.0.0-alpha.2
 	github.com/stretchr/testify v1.8.2
 )
 
 require (
 	fyne.io/systray v1.10.1-0.20230403195833-7dc3c09283d6 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.2 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fredbi/uri v0.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
@@ -19,19 +25,33 @@ require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	github.com/go-text/typesetting v0.0.0-20230405155246-bf9c697c6e16 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/goki/freetype v0.0.0-20220119013949-7a161fd3728c // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v2.0.6+incompatible // indirect
+	github.com/google/orderedcode v0.0.1 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
 	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/image v0.3.0 // indirect
 	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
