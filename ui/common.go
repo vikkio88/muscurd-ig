53759be37870b11eddef7011bb79cdd3e3630fc2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-05-18 20:22:45 +0100

    added flash message to common ui

--- ui/common.go
+++ ui/common.go
@@ -3,8 +3,11 @@ package ui
 import (
 	"image/color"
 	"muscurdig/context"
+	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -21,8 +24,53 @@ func h2(text string) *canvas.Text {
 	return txt
 }
 
+func errorMessage(errorMsg string, textItem *canvas.Text) {
+	flashMessage(
+		errorMsg, textItem,
+		time.Millisecond*800,
+		color.RGBA{255, 0, 50, 255},
+	)
+
+}
+func successMessage(msg string, textItem *canvas.Text) {
+	flashMessage(
+		msg, textItem,
+		time.Millisecond*800,
+		color.RGBA{0, 255, 50, 255},
+	)
+
+}
+func flashMessage(msg string, textItem *canvas.Text, duration time.Duration, color color.Color) {
+	textItem.Color = color
+	textItem.Text = msg
+	go func() {
+		time.Sleep(duration)
+		textItem.Text = ""
+	}()
+}
+
+func newFlashTxtPlaceholder() *canvas.Text {
+	return canvas.NewText("", color.White)
+}
+
 func backButton(ctx *context.AppContext, route context.AppRoute) *widget.Button {
 	return widget.NewButtonWithIcon("", theme.NavigateBackIcon(), func() {
 		ctx.NavigateTo(route)
 	})
+}
+
+func rightAligned(object fyne.CanvasObject) *fyne.Container {
+	return container.NewBorder(nil, nil, nil, object)
+}
+
+func leftAligned(object fyne.CanvasObject) *fyne.Container {
+	return container.NewBorder(nil, nil, object, nil)
+}
+
+func topAligned(object fyne.CanvasObject) *fyne.Container {
+	return container.NewBorder(object, nil, nil, nil)
+}
+
+func bottomAligned(object fyne.CanvasObject) *fyne.Container {
+	return container.NewBorder(nil, object, nil, nil)
 }
